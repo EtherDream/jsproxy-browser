255463c2073626c219424f9e4b068fd2553961ee
zjcqoo <zjcqoo@gmail.com>
2019-06-11 15:44:20 +0800

    新调整测试

--- www/sw.js
+++ www/sw.js
@@ -1,1 +1,1 @@
-importScripts(__FILE__='https://cdn.jsdelivr.net/gh/etherdream/jsproxy@gh-pages/assets/bundle.758a491e.js')
+importScripts(__FILE__='https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io/assets/bundle.758a491e.js')
