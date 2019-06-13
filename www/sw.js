e7692e960e33251885cd8f3b5c62baf6ccfbb148
zjcqoo <zjcqoo@gmail.com>
2019-06-13 09:30:06 +0800

    小调整

--- www/sw.js
+++ www/sw.js
@@ -1,1 +1,1 @@
-importScripts(__FILE__='https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io/assets/bundle.758a491e.js')
+importScripts(__FILE__='https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/bundle.91ae9a07.js')
