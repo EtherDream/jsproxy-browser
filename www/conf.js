d0d85d03c5575ab018562553b603b6af066feff6
qiqi <timqi@qq.com>
2019-11-02 17:08:43 +0800

    publish gh-pages

--- www/conf.js
+++ www/conf.js
@@ -84,7 +84,7 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/timqi/jsproxy-browser@gh-pages/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   // assets_cdn: 'assets/',
